ba74b1b5c00fc0d8ae93c54f0e89eb45d4ad9922
Darren Carver-Balsiger <darren.Carver-balsiger@ten10.com>
2020-06-23 12:06:16 +0100

    buy journey done

--- src/test/java/stepdefs/BuyJourneyStepDef.java
+++ src/test/java/stepdefs/BuyJourneyStepDef.java
@@ -2,6 +2,10 @@ package stepdefs;
 
 
 import config.DriverFactory;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import org.openqa.selenium.WebDriver;
 import pageObjects.*;
 
@@ -16,9 +20,9 @@ public class BuyJourneyStepDef {
     private ContactUsPage contactuspage = new ContactUsPage();
     private CheckoutPage checkoutPage = new CheckoutPage();
 
-    @Given ("^user is on the \"([^\"]*)\" page$")
+    @Given("^user is on the \"([^\"]*)\" page$")
     public void homepageSetUp(String page) {
-        driver.manage().deleteAllCookies();
+        homepage.clearCookies();
         switch (page){
             case "home":
                 homepage.goTo();
@@ -28,6 +32,9 @@ public class BuyJourneyStepDef {
                 homepage.goTo();
                 homepage.maximiseBrowserWindow();
                 productPage.navigatetoProductPage();
+                break;
+            case "dresses":
+                homepage.clickDresses();
                 break;
             default:
                 throw new IllegalArgumentException("Unrecognised Page Provided");
@@ -56,7 +63,7 @@ public class BuyJourneyStepDef {
         homepage.addItemToCart();
     }
 
-    @Then ("^verify product has been added to cart$")
+    @Then("^product has been added to cart$")
     public void verifyItemInCart() {
         homepage.addedToCart();
     }
@@ -68,11 +75,151 @@ public class BuyJourneyStepDef {
         productPage.selectColour();
     }
 
-    @And ("^clicks add to cart$")
+    @And("^clicks add to cart$")
     public void addToCart() {
         basketpage.addToCart();
     }
 
+    @And("^user is signed in$")
+    public void signIn() {
+        homepage.navigateToSignInPage();
+        signInPage.loginBuyJourneyTest();
+    }
+
+    @When("^user fills out message form on details page$")
+    public void fillOutMessage() {
+        orderspage.clickOrders();
+        orderspage.clickDetails();
+        orderspage.selectProductOnOrdersPage();
+        orderspage.writeMessage();
+    }
+
+    @Then("^message is sent off$")
+    public void verifyMessageSent() {
+        orderspage.verifyMessageSent();
+    }
+
+    @When("^filters are selected$")
+    public void selectFilters() {
+        womenPage.clickSize();
+        womenPage.clickColour();
+        womenPage.clickPrice();
+    }
+
+    @Then("^correct product is displayed$")
+    public void verifyProductDisplayed() {
+        womenPage.verifyCorrectProductDisplayed();
+    }
+
+    @When("^the user clears filters$")
+    public void clearFilters() {
+        womenPage.clearAll();
+        womenPage.refreshPage();
+    }
+
+    @Then("^all products are displayed$")
+    public void verifyAllProductsDisplayed() {
+        womenPage.verifyAllProducts();
+    }
+
+    @When("^user completes the newsletter form with a valid email address$")
+    public void completeNewsletterForm() {
+        homepage.subscribe();
+    }
+
+    @Then("^a success message is displayed$")
+    public void subscribeSuccess() {
+        homepage.verifySubscribe();
+    }
+
+    @When("^the user completes the contact us form$")
+    public void contactUsForm() {
+        homepage.clickContactUs();
+        contactuspage.enterMessage();
+        contactuspage.clickSend();
+    }
+
+    @Then("^a confirmation message is displayed$")
+    public void confirmationMessage() throws InterruptedException {
+        contactuspage.verifyMessageSent();
+    }
+
+    @And("^the user adds product to cart$")
+    public void addProductToCart(){
+        homepage.addItemToCart();
+    }
+
+    @When("^user goes to summer dress page")
+    public void goToSummerDressPage() {
+        homepage.clickSummerDresses();
+    }
+
+    @When("^the user searches for product$")
+    public void searchProduct() {
+        homepage.searchForDress();
+    }
+
+    @When("^the user continues and buys another item$")
+    public void buySecondItem() {
+        homepage.clickContinueShopping();
+        homepage.addDifferentItemToCart();
+        homepage.clickCheckout();
+    }
+
+    @Then("^cart page has both items$")
+    public void checkCartPage() {
+        basketpage.verifyProductCountUpdated();
+    }
+
+    @When("^user attempts to buy large quantity of product$")
+    public void buyLotOfProduct() {
+        homepage.clickProduct();
+        productPage.inputLargeQuantity();
+    }
+
+    @Then("^out of stock message appears$")
+    public void outOfStockMessage() {
+        productPage.verifyOutOfStock();
+    }
+
+    @And("^add to cart button is disabled$")
+    public void cartButtonDisabled() {
+        productPage.verifyButtonDisabled();
+    }
+
+    @When("^user selects product with different options$")
+    public void selectProduct() {
+        productPage.navigatetoProductPage();
+    }
+
+    @Then("^product available with different options message is displayed$")
+    public void differentOptionsMessage() {
+        productPage.verifyOtherOptionsAvailable();
+    }
+
+    @When("^user reorders item$")
+    public void reorderItem() {
+        orderspage.clickOrders();
+        orderspage.clickReorder();
+    }
+
+    @And("^user completes purchase$")
+    public void completePurchase() {
+        checkoutPage.deliveryAddressSectionDisplayed();
+        checkoutPage.clickProceedToNextSection();
+        checkoutPage.shippingPageDisplayed();
+        checkoutPage.enterShippingComment();
+        checkoutPage.clickProceedToNextSection();
+        checkoutPage.paymentPageDisplayed();
+        checkoutPage.clickPayByBankWire();
+        checkoutPage.agreeToTerms();
+        checkoutPage.confirmOrder();
+    }
+
+    @Then("^purchase is successful$")
+    public void purchaseSuccessful() {
+        checkoutPage.orderConfirmationDisplayed();
+    }
 
 }
 
--- target/test-classes/features/buyjourney.feature
+++ target/test-classes/features/buyjourney.feature
@@ -1,7 +1,83 @@
 Feature: BuyJourney
 
   Scenario: Select Item from homepage
-    Given the user is on the home page
+    Given user is on the "home" page
     When the user goes to a product
     And clicks more
     Then the Product Page is displayed
+
+  Scenario: Add Item to Cart on homepage
+    Given user is on the "home" page
+    When the user clicks add to cart
+    Then product has been added to cart
+
+  Scenario: Add Item to Cart on productpage
+    Given user is on the "product" page
+    When the user specifies exact purchase
+    And clicks add to cart
+    Then verify product has been added to cart
+
+  Scenario: Add Message to Previous Order
+    Given user is on the "home" page
+    And user is signed in
+    When user fills out message form on details page
+    Then message is sent off
+
+  Scenario: Filters Display Correct Products
+    Given user is on the "dresses" page
+    When filters are selected
+    Then correct product is displayed
+
+  Scenario: Clear Filters Restores Products
+    Given user is on the "dresses" page
+    And filters are selected
+    When  the user clears filters
+    Then all products are displayed
+
+  Scenario: Subscribe to Newsletter
+    Given user is on the "home" page
+    When user completes the newsletter form with a valid email address
+    Then a success message is displayed
+
+  Scenario: Send Message Through Contact Us Page
+    Given user is on the "home" page
+    And user is signed in
+    When the user completes the contact us form
+    Then a confirmation message is displayed
+
+  Scenario: Buy Summer Dress via Menu
+    Given user is on the "home" page
+    When user goes to summer dress page
+    And the user adds product to cart
+    Then product has been added to cart
+
+  Scenario: Add Item via Search
+    Given user is on the "home" page
+    When the user searches for product
+    And the user adds product to cart
+    Then product has been added to cart
+
+  Scenario: Buy Second Item
+    Given user is on the "home" page
+    And the user adds product to cart
+    When the user continues and buys another item
+    Then cart page has both items
+
+  Scenario: Order Large Quantity of Item
+    Given user is on the "home" page
+    When user attempts to buy large quantity of product
+    Then out of stock message appears
+    And add to cart button is disabled
+
+  Scenario: Product Available in Different Options
+    Given user is on the "home" page
+    When user selects product with different options
+    Then product available with different options message is displayed
+
+  Scenario: Reorder Product
+    Given user is on the "home" page
+    And user is signed in
+    When user reorders item
+    And user completes purchase
+    Then purchase is successful
+
